272ec0f21347890d8601768936d68085ca636157
Ondrej Zavodny <ondrej@zavodny.net>
2020-12-25 08:24:58 +0000

    Initial

---
+++ configuration.nix
@@ -0,0 +1,189 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hammond"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+  nixpkgs.config.allowUnfree = true;  
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlo1.useDHCP = true;
+
+  networking.firewall.allowedTCPPortRanges = [ { from = 25565; to = 25565; } ];
+  networking.firewall.allowedUDPPortRanges = [ { from = 24642; to = 24642; } ];
+  
+hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  hardware.pulseaudio = {
+    enable = true;
+
+    # NixOS allows either a lightweight build (default) or full build of PulseAudio to be installed.
+    # Only the full build has Bluetooth support, so it must be selected here.
+    package = pkgs.pulseaudioFull;
+  };
+
+  nixpkgs.config.pulseaudio = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      overpass
+      fira-code
+      noto-fonts
+      iosevka
+      
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Fira Code" ];
+        sansSerif = [ "Overpass" ];
+        serif     = [ "Noto Serif" ];
+      };
+    };
+  };
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
+  # Enable the Plasma 5 Desktop Environment.
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver = {
+  #  videoDrivers = [ "nvidia" ];
+    enable = true;
+    layout = "us";
+    desktopManager = {
+      xfce = {
+        enable = true;
+        enableXfwm = false;
+        noDesktop = true;
+      };
+      
+      xterm.enable = false;
+    };
+    
+    
+    windowManager.bspwm = {
+      enable = true;
+      configFile = "/home/ozavodny/new_infra/common/bspwm/bspwmrc";
+      sxhkd.configFile = "/home/ozavodny/new_infra/common/bspwm/sxhkdrc";
+      
+    };
+
+    	
+    displayManager = {
+      defaultSession = "xfce+bspwm";
+    };
+
+  };
+
+  programs.light.enable = true;
+
+  security.sudo.enable = true;
+  security.sudo.extraRules = [
+  { users = [ "ALL" ];
+      commands = [ {
+       command = "/run/current-system/sw/bin/light";
+       options = [ "NOPASSWD" ]; 
+    } ];
+    }
+  ];
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ozavodny = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ]; # Enable ‘sudo’ for the user.
+    createHome = true;
+    home = "/home/ozavodny";
+    shell = "/run/current-system/sw/bin/bash";
+  };
+
+   # List packages installed in system profile. To search, run:
+   # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     wget vim
+     firefox
+     neofetch
+     terminator
+     polybar
+     rofi
+     pcmanfm
+     pamixer 
+     git
+];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
