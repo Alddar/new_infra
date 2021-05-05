a028e388b16337b54f1aef241805d906b6d160df
Ondrej Zavodny <ondrej@zavodny.net>
2021-05-05 02:31:39 +0200

    fixed path issue

--- configuration/genji/configuration.nix
+++ configuration/genji/configuration.nix
@@ -1,8 +1,8 @@
 { config, options, ... }:
 {
   imports = [
-    "../common/basic.nix"
-    "../rpi/3bplus.nix"
+    ../common/basic.nix
+    ../rpi/3bplus.nix
   ];
 
   basic.user.extraGroups = [ "wheel" ];
