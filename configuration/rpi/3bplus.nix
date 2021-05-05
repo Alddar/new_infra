a3d1844df3530e61922922b9b1a30936b94b51e5
Ondrej Zavodny <ondrej@zavodny.net>
2021-05-05 02:53:08 +0200

    fixed rpi issues

--- configuration/rpi/3bplus.nix
+++ configuration/rpi/3bplus.nix
@@ -5,12 +5,7 @@
   boot.kernelPackages = pkgs.linuxPackages;
 
   boot.kernelParams = ["cma=256M"];
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 3;
-  boot.loader.raspberryPi.uboot.enable = true;
-  boot.loader.raspberryPi.firmwareConfig = ''
-    gpu_mem=256
-  '';
+
   environment.systemPackages = with pkgs; [
     libraspberrypi
   ];
