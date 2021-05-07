33238f7ce7e0408eec38e46b6456ca7d27ac7afb
Ondrej Zavodny <ondrej@zavodny.net>
2021-05-07 16:12:50 +0200

    added things

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ git add .
 git commit -m "added things"
 git push
 
-ssh ozavodny@genji 'cd ~/new_infra ; git pull ; sudo nixos-rebuild switch'
+ssh ozavodny@genji 'cd ~/infra ; git pull ; sudo nixos-rebuild switch'
 
