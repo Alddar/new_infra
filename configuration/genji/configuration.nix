74aadc450f746a3d693d47a355241650646df5b0
Your Name <you@example.com>
2021-05-16 15:25:25 +0200

    added things

--- configuration/genji/configuration.nix
+++ configuration/genji/configuration.nix
@@ -13,4 +13,25 @@
     # Configure basic SSH access
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    # other Nginx options
+    virtualHosts."jellyfin.genji.local" =  {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://192.168.0.2:8096";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+};
 }
