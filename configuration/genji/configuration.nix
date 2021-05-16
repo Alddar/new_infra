bdabd210700718b4128efb05f39247c4d57a4b96
Your Name <you@example.com>
2021-05-17 00:25:26 +0200

    added things

--- configuration/genji/configuration.nix
+++ configuration/genji/configuration.nix
@@ -14,14 +14,43 @@
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
 
+  services.coredns.enable = true;
+  services.coredns.config =
+    ''
+      . {
+        # Cloudflare and Google
+        forward . 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4
+        cache
+      }
+
+      local {
+        template IN A  {
+            answer "{{ .Name }} 0 IN A 127.0.0.1"
+        }
+      }
+    '';
+
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     # other Nginx options
-    virtualHosts."genji.local" =  {
+    virtualHosts."jellyfin.local" =  {
       locations."/" = {
         proxyPass = "http://192.168.0.2:8096";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+
+    virtualHosts."deluge.local" =  {
+      locations."/" = {
+        proxyPass = "http://192.168.0.2:8112";
         proxyWebsockets = true; # needed if you need to use WebSocket
         extraConfig =
           # required when the target is also TLS server with multiple hosts
