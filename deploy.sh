d8f991032f93a63ba3ab57376c00535c1fb04ec2
Your Name <you@example.com>
2021-05-18 12:42:25 +0200

    added things

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ echo -e "\nPushing...\n"
 
 git push
 
-ssh ozavodny@genji 'cd ~/infra ; echo -e "\nPulling...\n" ; git pull ; echo -e "\nSwitching...\n" ; sudo nixos-rebuild switch'
+ssh ozavodny@192.168.0.3 'cd ~/infra ; echo -e "\nPulling...\n" ; git pull ; echo -e "\nSwitching...\n" ; sudo nixos-rebuild switch'
